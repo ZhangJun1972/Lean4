9e89dbaf6413632deb0f22d55e2a23095af510f1
Rob23oba <robin.arnez@web.de>
2026-02-08 17:35:00 +0100

    improve second optimization attempt

--- src/Lean/Compiler/LCNF/Passes.lean
+++ src/Lean/Compiler/LCNF/Passes.lean
@@ -101,7 +101,7 @@ def builtinPassManager : PassManager := {
     Pass.checkMeta,
     pullInstances,
     cse (shouldElimFunDecls := false),
-    simp,
+    simp { simpCtor := false },
     floatLetIn,
     findJoinPoints,
     pullFunDecls,
